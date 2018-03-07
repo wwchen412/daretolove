abf9290feb73dff68c55658c4b2acb28256f1c6a
wwchen <wwchen412@gmail.com>
2018-03-07 17:19:15 +0800

    裝上cookie

--- script/all.js
+++ script/all.js
@@ -10,7 +10,21 @@ window.addEventListener("scroll", function() {
   }
 });
 
+// 
+
 $(function() {
+   if ($.cookie("name") == null) {
+     var _hostname = window.location.hostname; //取得網址
+     $.cookie("name", _hostname, { expires: 7, path: "/" });
+     $.cookie("count", 1, { expires: 7, path: "/" });
+     window.location.href = "warning.html";
+   } else {
+     var _hostname = $.cookie("name");
+     var _count = $.cookie("count");
+     // console.log("歡迎來到" + _hostname + "，你是第 " + (++_count) + " 次拜訪本小站。");
+     // 把加 1 之後的 count 寫回記錄中
+     $.cookie("count", _count, { expires: 7, path: "/" });
+   }
   $("#menu").click(function() {
     $("nav").toggleClass("active");
     $("body").toggleClass("active");
